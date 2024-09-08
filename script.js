e96a10efc144cd55c9d2da054bc3bbc4baf9b637
lukmanAkhmad <145940997+lukmanAkhmad@users.noreply.github.com.>
2024-09-08 10:55:46 +0700

    feat: add button resize

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
-let container = document.querySelector("#container");
+const button = document.createElement("button");
+let container = document.createElement("div");
+container.setAttribute("id", "container");
+
+let body = document.querySelector("body");
+
+body.appendChild(button);
+body.appendChild(container);
+
+button.textContent = "Resize";
+button.addEventListener("click", createPopup)
+
+function createPopup() {
+
+    let popup = +prompt("Entered number 1 - 100");
+    container.textContent = "";
+    if (popup > 100 || popup < 0) {
+        alert("Entered number 1 - 100")
+        createSquare(16);
+    } else {
+        createSquare(popup);
+    }
+}
+
 
 function createSquare(numberOfDivToCreate) {
     // 600 comes from the width and height of the container
